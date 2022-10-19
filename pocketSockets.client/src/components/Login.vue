2382f19c77f2df354f84b45bb5b663fc2cf9f441
Jake Overall <joverall22@gmail.com>
2022-10-19 00:42:12 -0600

    login nav fix

--- pocketSockets.client/src/components/Login.vue
+++ pocketSockets.client/src/components/Login.vue
@@ -1,39 +1,29 @@
 <template>
   <span class="navbar-text">
-    <button
-      class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0"
-      @click="login"
-      v-if="!user.isAuthenticated"
-    >
+    <button class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0" @click="login"
+      v-if="!user.isAuthenticated">
       Login
     </button>
-
-    <div class="dropdown my-2 my-lg-0" v-else>
-      <div
-        class="dropdown-toggle selectable"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-        id="authDropdown"
-      >
-        <div v-if="account.picture || user.picture">
-          <img
-            :src="account.picture || user.picture"
-            alt="account photo"
-            height="40"
-            class="rounded"
-          />
-          <span class="mx-3 text-success lighten-30">{{ account.name || user.name }}</span>
-        </div>
-      </div>
-      <div class="dropdown-menu p-0 list-group w-100" aria-labelledby="authDropdown">
-        <router-link :to="{ name: 'Account' }">
-          <div class="list-group-item list-group-item-action hoverable">
-            Manage Account
+    <div v-else>
+      <div class="dropdown dropstart my-2 my-lg-0">
+        <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          <div v-if="account.picture || user.picture">
+            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
           </div>
-        </router-link>
-        <div class="list-group-item list-group-item-action hoverable text-danger" @click="logout">
-          <i class="mdi mdi-logout"></i>
-          logout
+        </div>
+        <div class="dropdown-menu dropdown-menu-lg-left p-0" aria-labelledby="authDropdown">
+          <div class="list-group">
+            <router-link :to="{ name: 'Account' }">
+              <div class="list-group-item dropdown-item list-group-item-action">
+                Manage Account
+              </div>
+            </router-link>
+            <div class="list-group-item dropdown-item list-group-item-action text-danger selectable" @click="logout">
+              <i class="mdi mdi-logout"></i>
+              logout
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -61,18 +51,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dropdown-menu {
-  user-select: none;
-  display: block;
-  transform: scale(0);
-  transition: all 0.15s linear;
-}
 
-.dropdown-menu.show {
-  transform: scale(1);
-}
-
-.hoverable {
-  cursor: pointer;
-}
 </style>
